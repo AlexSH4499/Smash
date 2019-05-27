6579c0dcced020923f1d70881d5d52697dd1b49d
Alejandro Rodriguez Natal <rodrigueznatalandy@gmail.com>
2019-05-27 01:26:08 -0400

    I f'ed up, fixed the project structure. Now I have to redo all necessary changes to host on heroku

--- tierlistsite/settings.py
+++ tierlistsite/settings.py
@@ -19,8 +19,7 @@ DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -127,7 +126,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 ALLOWED_HOSTS = ['*']
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 #CORS_ALLOW_CREDENTIALS = False
 #CORS_ORIGIN_WHITELIST = 'http://localhost:3000',
 CORS_ORIGIN_ALLOW_ALL = True
